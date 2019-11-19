40f4dd3b73d388ebdc82a22221c7be1234753ee8
Milan Straka <milan@strakovi.com>
2019-11-19 09:10:43 +0100

    Preliminary version of kernel_linear_regression.

---
+++ labs/05/kernel_linear_regression.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.metrics
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--examples", default=50, type=int, help="Number of examples")
+    parser.add_argument("--kernel", default="rbf", type=str, help="Kernel type [poly|rbf]")
+    parser.add_argument("--kernel_degree", default=3, type=float, help="Degree for poly kernel")
+    parser.add_argument("--kernel_gamma", default=1.0, type=float, help="Gamma for poly and rbf kernel")
+    parser.add_argument("--iterations", default=50, type=int, help="Number of training iterations")
+    parser.add_argument("--l2", default=0.001, type=float, help="L2 regularization weight")
+    parser.add_argument("--learning_rate", default=0.01, type=float, help="Learning rate")
+    parser.add_argument("--plot", default=False, action="store_true", help="Plot progress")
+    parser.add_argument("--seed", default=42, type=int, help="Random seed")
+    args = parser.parse_args()
+
+    # Set random seed
+    np.random.seed(args.seed)
+
+    # Generate an artifical regression dataset
+    train_data = np.linspace(-5, 5, args.examples)
+    train_target = np.sin(train_data) + np.random.normal(scale=0.25, size=args.examples) + 1
+
+    test_data = np.linspace(-10, 10, 2 * args.examples)
+    test_target = np.sin(test_data) + 1
+
+    coefs = np.zeros(args.examples)
+
+    # TODO: Perform `iterations` of SGD-like updates, but in dual formulation
+    # using `coefs` as weights of individual training examples.
+    #
+    # We assume the primary formulation of our model is
+    #   y = phi(x)^T w + bias
+    # and the loss in the primary problem is MSE with L2 regularization:
+    #   L = sum_{i=1}^N [(target_i - phi(x_i)^T w - bias)^2] + 1/2 * l2 * w^2
+    #
+    # For bias use explicitly the average of training targets, and do not update
+    # it futher during training.
+    #
+    # Instead of using feature map phi directly, we use given kernel computing
+    #   K(x, y) = phi(x)^T phi(y)
+    # We consider the following kernels:
+    # - poly: K(x, y; degree, gamma) = (gamma * x^T y + 1) ^ degree
+    # - rbf: K(x, y; gamma) = exp^{- gamma * ||x - y||^2}
+    #
+    # After each update print RMSE both on training and testing data.
+    for iteration in range(args.iterations):
+        # TODO
+
+        print("Iteration {}, train RMSE {:.2f}, test RMSE {:.2f}".format(
+            iteration + 1,
+            # RMSE on train data,
+            # RMSE on test data,
+        ))
+
+    if args.plot:
+        test_predictions = #TODO
+
+        plt.plot(train_data, train_target, "bo", label="Train targets")
+        plt.plot(test_data, test_target, "ro", label="Test targets")
+        plt.plot(test_data, test_predictions, "g-", label="Predictions")
+        plt.legend(loc="upper left")
+        plt.show()
