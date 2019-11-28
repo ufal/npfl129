ac0e0c23d6afe5c5f04d3f6369d066cf6346c92d
Milan Straka <milan@strakovi.com>
2019-11-28 16:25:23 +0100

    Update decision_tree_classification assigment.

--- labs/06/decision_tree_classification.py
+++ labs/06/decision_tree_classification.py
@@ -31,18 +31,29 @@ if __name__ == "__main__":
 
     # TODO: Create a decision tree on the trainining data.
     #
-    # For each node, predict the most frequent class (and the one with smallest
-    # index if there are several such classes).
+    # - For each node, predict the most frequent class (and the one with
+    # smallest index if there are several such classes).
+    #
+    # - When splitting a node, consider the features in sequential order, then
+    #   for each feature consider all possible split points ordered in ascending
+    #   value, and perform the first encountered split descreasing the criterion
+    #   the most. Each split point is an average of two nearest feature values
+    #   of the instances corresponding to the given node (i.e., for three instances
+    #   with values 1, 7, 3, the split points are 2 and 5).
+    #
+    # - Allow splitting a node only if:
+    #   - when `args.max_depth` is not None, its depth must be at most `args.max_depth`;
+    #     depth of the root node is zero;
+    #   - there are at least `args.min_to_split` corresponding instances;
+    #   - the criterion value is not zero.
     #
     # - When `args.max_leaves` is None, use recursive (left descendants first, then
-    #   right descendants) approach, splitting every node if `args.max_depth`
-    #   and `args.min_to_split` constraints are valid. When splitting, consider
-    #   the features in sequential order, then for each feature consider all
-    #   possible split points ordered in ascending value, and perform the
-    #   first encountered split descreasing the criterion the most.
-    # - When `args.max_leaves` is not None, always split a node where the
-    #   overall criterion value decreases the most (c_left + c_right - c_node).
-    #   If there are several such nodes, choose the one which was created sooner.
+    #   right descendants) approach, splitting every node if the constraints are valid.
+    #   Otherwise (when `args.max_leaves` is not None), always split a node where the
+    #   constraints are valid and the overall criterion value (c_left + c_right - c_node)
+    #   decreases the most. If there are several such nodes, choose the one
+    #   which was created sooner (a left child is considered to be created
+    #   before a right child).
 
     # TODO: Finally, measure the training and testing accuracy.
     print("Train acc: {:.1f}%".format(100 * train_accuracy))
--- tasks/decision_tree_classification.md
+++ tasks/decision_tree_classification.md
@@ -1,8 +1,16 @@
 ### Assignment: decision_tree_classification
 #### Date: Deadline: Dec 08, 23:59
-#### Points: 6 points
+#### Points: 8 points
+#### Examples: decision_tree_classification_examples
 
 Starting with the [decision_tree_classification.py](https://github.com/ufal/npfl129/tree/master/labs/06/decision_tree_classification.py),
 implement construction of a classification decision tree, supporting both
 `gini` and `entropy` criteria, and `max_depth`, `min_to_split` and `max_leaves`
 constraints.
+
+In this assignment, you will get partial points during ReCodEx evaluation,
+depending on which argument values your solution support.
+
+#Examples Start: decision_tree_classification_examples
+#Include: decision_tree_classification_examples.md
+#Examples End:
