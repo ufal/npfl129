4d0454120daec5a1af1f6917c79904019be88dc1
Milan Straka <milan@strakovi.com>
2019-12-03 08:07:39 +0100

    Add isnt_it_ironic assignment.

---
+++ labs/07/isnt_it_ironic.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import argparse
+import lzma
+import pickle
+import os
+import urllib.request
+import sys
+import zipfile
+
+import numpy as np
+
+import sklearn.metrics
+
+class Dataset:
+    def __init__(self,
+                 name="isnt_it_ironic.train.zip",
+                 url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/1920/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name), file=sys.stderr)
+            urllib.request.urlretrieve(url + name, filename=name)
+
+        # Load the dataset and split it into `data` and `target`.
+        self.data = []
+        self.target = []
+
+        with zipfile.ZipFile(name, "r") as dataset_file:
+            with dataset_file.open(name.replace(".zip", ".txt"), "r") as train_file:
+                for line in train_file:
+                    label, text = line.decode("utf-8").rstrip("\n").split("\t")
+                    self.data.append(text)
+                    self.target.append(int(label))
+        self.target = np.array(self.target, np.int32)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_path", default="isnt_it_ironic.model", type=str, help="Model path")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    # Set random seed
+    np.random.seed(args.seed)
+
+    # Load the dataset, downloading it if required
+    train = Dataset()
+
+    # TODO: Train the model.
+
+    # TODO: The trained model needs to be saved. All sklearn models can
+    # be serialized and deserialized using the standard `pickle` module.
+    # Additionally, we also compress the model.
+    #
+    # To save a model, open a target file for binary access, and use
+    # `pickle.dump` to save the model to the opened file:
+    with lzma.open(args.model_path, "wb") as model_file:
+        pickle.dump(model, model_file)
+
+# The `recodex_predict` is called during ReCodEx evaluation (there can be
+# several Python sources in the submission, but exactly one should contain
+# a `recodex_predict` method).
+def recodex_predict(data):
+    # The `data` is a Python list containing tweets as `str`ings.
+
+    args = parser.parse_args([])
+
+    # TODO: Predict target values for the given data.
+    #
+    # You should probably start by loading a model. Start by opening the model
+    # file for binary read access and then use `pickle.load` to deserialize the
+    # model from the stored binary data:
+    with lzma.open(args.model_path, "rb") as model_file:
+        model = pickle.load(model_file)
+
+    # TODO: Return the predictions as a Pythoh list or Numpy array of
+    # binary labels of the tweets.
--- lectures/lecture07.md
+++ lectures/lecture07.md
@@ -1,5 +1,5 @@
-### Lecture: 7. Soft-margin SVM, SMO Algorithm, Decision Trees
-#### Date: Nov 25
-#### Slides: https://ufal.mff.cuni.cz/~straka/courses/npfl129/1920/slides/?06
-#### Reading: https://ufal.mff.cuni.cz/~straka/courses/npfl129/1920/slides.pdf/npfl129-06.pdf,PDF Slides
-#### Lecture assignment: decision_tree_classification
+### Lecture: 7. SMO Algorithm
+#### Date: Dec 02
+#### Slides: https://ufal.mff.cuni.cz/~straka/courses/npfl129/1920/slides/?07
+#### Reading: https://ufal.mff.cuni.cz/~straka/courses/npfl129/1920/slides.pdf/npfl129-07.pdf,PDF Slides
+#### Lecture assignment: isnt_it_ironic
