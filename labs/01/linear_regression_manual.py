d1ecdc7d3e1435ba956a7af64ac41ba8d866b8ff
Milan Straka <milan@strakovi.com>
2020-10-06 22:24:42 +0200

    Add linear_regression_manual.

---
+++ labs/01/linear_regression_manual.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+
+import numpy as np
+import sklearn.datasets
+import sklearn.model_selection
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+parser.add_argument("--test_size", default=50, type=int, help="Test size to use")
+
+def main(args):
+    # Load Boston housing dataset
+    dataset = sklearn.datasets.load_boston()
+
+    # The input data are in dataset.data, targets are in dataset.target.
+
+    # If you want to learn about the dataset, uncomment the following line.
+    # print(dataset.DESCR)
+
+    # TODO: Append a new feature to all input data, with value "1"
+
+    # TODO: Split the dataset into a train set and a test set.
+    # Use `sklearn.model_selection.train_test_split` method call, passing
+    # arguments `test_size=args.test_size, random_state=args.seed`.
+
+    # TODO: Solve the linear regression using the algorithm from the lecture,
+    # explicitly computing the matrix inverse (using `np.linalg.inv`).
+
+    # TODO: Predict target values on the test set
+
+    # TODO: Compute root mean square error on the test set predictions
+    rmse = None
+
+    return rmse
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    rmse = main(args)
+    print("{:.2f}".format(rmse))
---
+++ tasks/linear_regression_manual.md
@@ -0,0 +1,16 @@
+### Assignment: linear_regression_manual
+#### Date: Deadline: Oct 20, 23:59
+#### Points: 3 points
+#### Examples: linear_regression_manual_examples
+
+Starting with the
+[linear_regression_manual.py](https://github.com/ufal/npfl129/tree/master/labs/01/linear_regression_manual.py)
+template, solve a linear regression problem using the algoritm from the lecture
+which explicitly computes the matrix inversion. Then compute root mean square
+error on the test set.
+### Examples Start: linear_regression_manual_examples
+- `python3 linear_regression_manual.py `
+```
+3.89
+```
+#### Examples End:
