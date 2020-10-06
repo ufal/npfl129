4654c576ee2c3448a456869c98fd4010dbac2dc8
Milan Straka <milan@strakovi.com>
2020-10-07 00:32:38 +0200

    Add linear_regression_features.

---
+++ labs/01/linear_regression_features.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import argparse
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn.linear_model
+import sklearn.metrics
+import sklearn.model_selection
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_size", default=40, type=int, help="Data size")
+parser.add_argument("--range", default=3, type=int, help="Feature order range")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+
+def main(args):
+    # Create the data
+    xs = np.linspace(0, 7, num=args.data_size)
+    ys = np.sin(xs) + np.random.RandomState(args.seed).normal(0, 0.2, size=args.data_size)
+
+    rmses = []
+    for order in range(1, args.range + 1):
+        # TODO: Create features of x^1, ..., x^order.
+
+        # TODO: Split the data into a train set and a test set.
+        # Use `sklearn.model_selection.train_test_split` method call, passing
+        # arguments `test_size=args.test_size, random_state=args.seed`.
+
+        # TODO: Fit a linear regression model using `sklearn.linear_model.LinearRegression`.
+
+        # TODO: Predict targets on the test set using the trained model.
+
+        # TODO: Compute root mean square error on the test set predictions
+        rmse = None
+
+        rmses.append(rmse)
+
+    return rmses
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    rmses = main(args)
+    for order, rmse in enumerate(rmses):
+        print("Maximum feature order {}: {:.2f} RMSE".format(order + 1, rmse))
---
+++ tasks/linear_regression_features.md
@@ -0,0 +1,34 @@
+### Assignment: linear_regression_features
+#### Date: Deadline: Oct 20, 23:59
+#### Points: 3 points
+#### Examples: linear_regression_features_examples
+
+Starting with the
+[linear_regression_features.py](https://github.com/ufal/npfl129/tree/master/labs/01/linear_regression_features.py)
+template, use `scikit-learn` to train a model of a 1D curve.
+
+Try using features $x^1, x^2, …, x^D$ for $D$ from 1 to a given range, and
+report RMSE of every such configuration.
+
+#### Examples Start: linear_regression_features_examples
+- `python3 linear_regression_features.py --data_size=10 --test_size=5 --range=5`
+```
+Maximum feature order 1: 0.74 RMSE
+Maximum feature order 2: 1.87 RMSE
+Maximum feature order 3: 0.53 RMSE
+Maximum feature order 4: 4.52 RMSE
+Maximum feature order 5: 1.70 RMSE
+```
+- `python3 linear_regression_features.py --data_size=50 --test_size=40 --range=9`
+```
+Maximum feature order 1: 0.63 RMSE
+Maximum feature order 2: 0.73 RMSE
+Maximum feature order 3: 0.31 RMSE
+Maximum feature order 4: 0.26 RMSE
+Maximum feature order 5: 1.22 RMSE
+Maximum feature order 6: 0.69 RMSE
+Maximum feature order 7: 2.39 RMSE
+Maximum feature order 8: 7.28 RMSE
+Maximum feature order 9: 201.70 RMSE
+```
+#### Examples End:
