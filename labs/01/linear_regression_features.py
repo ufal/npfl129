35be8842d06d0091ed4254aa5518b5b119024d54
Milan Straka <milan@strakovi.com>
2020-10-10 01:12:54 +0200

    Explicitly describe what arguments are set by ReCodEx.

--- labs/01/linear_regression_features.py
+++ labs/01/linear_regression_features.py
@@ -7,11 +7,13 @@ import sklearn.metrics
 import sklearn.model_selection
 
 parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--data_size", default=40, type=int, help="Data size")
 parser.add_argument("--range", default=3, type=int, help="Feature order range")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 parser.add_argument("--test_size", default=0.5, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+# If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
     # Create the data
--- labs/01/linear_regression_manual.py
+++ labs/01/linear_regression_manual.py
@@ -7,9 +7,11 @@ import sklearn.metrics
 import sklearn.model_selection
 
 parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 parser.add_argument("--test_size", default=0.1, type=lambda x:int(x) if x.isdigit() else float(x), help="Test set size")
+# If you add more arguments, ReCodEx will keep them with your default values.
 
 def main(args):
     # Load Boston housing dataset
