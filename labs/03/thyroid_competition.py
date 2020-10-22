94fa0c50662d1c548b69e0aea90bc2c045cdf167
Milan Straka <milan@strakovi.com>
2020-10-22 07:53:34 +0200

    Thyroid_competition template and updated description.

---
+++ labs/03/thyroid_competition.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import argparse
+import lzma
+import os
+import pickle
+import urllib.request
+
+import numpy as np
+
+class Dataset:
+    """Thyroid Dataset.
+
+    The dataset contains real medical data related to thyroid gland function,
+    classified either as normal or irregular (i.e., some thyroid disease).
+    The data consists of the following features in this order:
+    - 15 binary features
+    - 6 real-valued features
+
+    The target variable is binary, with 1 denoting a thyroid disease and
+    0 normal function.
+    """
+    def __init__(self,
+                 name="thyroid_competition.train.npz",
+                 url="https://ufal.mff.cuni.cz/~straka/courses/npfl129/2021/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name))
+            urllib.request.urlretrieve(url + name, filename=name)
+
+        # Load the dataset and return the data and targets.
+        dataset = np.load(name)
+        for key, value in dataset.items():
+            setattr(self, key, value)
+
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--predict", default=None, type=str, help="Run prediction on given data")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--model_path", default="thyroid_competition.model", type=str, help="Model path")
+
+def main(args):
+    if args.predict is None:
+        # We are training a model.
+        np.random.seed(args.seed)
+        train = Dataset()
+
+        # TODO: Train a model on the given dataset and store it in `model`.
+        model = None
+
+        # Serialize the model.
+        with lzma.open(args.model_path, "wb") as model_file:
+            pickle.dump(model, model_file)
+
+    else:
+        # Use the model and return test set predictions, as either a Python list or a NumPy array.
+        test = Dataset(args.predict)
+
+        with lzma.open(args.model_path, "rb") as model_file:
+            model = pickle.load(model_file)
+
+        # TODO: Generate `predictions` with the test set predictions.
+        predictions = None
+
+        return predictions
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
--- tasks/thyroid_competition.md
+++ tasks/thyroid_competition.md
@@ -4,7 +4,7 @@
 
 This assignment is a [competition task](#competitions). Your goal
 is to perform binary classification – given medical data with 15 binary and
-6 real attributes, predict whether thyroid is functioning normally or not.
+6 real-valued attributes, predict whether thyroid is functioning normally or not.
 The train set and test set consist of ~3.5k instances.
 
 The [thyroid_competition.py](https://github.com/ufal/npfl129/tree/master/labs/03/thyroid_competition.py)
@@ -13,5 +13,6 @@ Furthermore, it shows how to save a trained estimator and how to load it during
 prediction.
 
 The performance of your system is measured using _accuracy_ of correctly
-predicted examples. Note that you can use any number of **logistic and linear
-regression models** to solve this assignment.
+predicted examples and your goal is to achieve at least 96% accuracy.
+Note that you can use any number of **generalized linear models** with any
+regularization to solve this assignment (but no decision trees, MLPs, …).
