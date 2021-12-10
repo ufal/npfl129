89d02c3998f6c67008c6691293ef59c450dab446
Milan Straka <milan@strakovi.com>
2021-12-10 22:14:36 +0100

    Coorect a typo.
    Fixes #119.

--- labs/09/metric_correlation.py
+++ labs/09/metric_correlation.py
@@ -48,7 +48,7 @@ def main(args: argparse.Namespace) -> tuple[float, float]:
         # Bootstrap sample of the dataset
         sentences = generator.choice(data.sentences, size=len(data.sentences), replace=True)
 
-        # TODO: Append the averate of human ratings of `sentences` to `humans`.
+        # TODO: Append the average of human ratings of `sentences` to `humans`.
         human_ratings.append(...)
 
         # TODO: Compute TP, FP, FN counts of predicted edits in `sentences`
