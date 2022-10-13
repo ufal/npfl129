7fe803639dcba84b82c9db340b34a2a622a9a68d
Milan Straka <milan@strakovi.com>
2022-10-13 07:34:51 +0200

    Explicitly state that `--predict` is a path to a dataset.

--- labs/02/rental_competition.py
+++ labs/02/rental_competition.py
@@ -45,7 +45,7 @@ class Dataset:
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--predict", default=None, type=str, help="Run prediction on given data")
+parser.add_argument("--predict", default=None, type=str, help="Path to the dataset to predict")
 parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
 parser.add_argument("--seed", default=42, type=int, help="Random seed")
 # For these and any other arguments you add, ReCodEx will keep your default value.
