6e8b2136ed714e12237ce2c1412e73ca04cb805a
Milan Straka <milan@strakovi.com>
2022-12-10 14:14:37 +0100

    Minor improvements in the `args.max_leaves != None` case.

--- labs/09/decision_tree.py
+++ labs/09/decision_tree.py
@@ -41,14 +41,16 @@ def main(args: argparse.Namespace) -> tuple[float, float]:
     #   with values 1, 7, 3, 3, the split points are 2 and 5).
     #
     # - Allow splitting a node only if:
-    #   - when `args.max_depth` is not `None`, its depth must be less than `args.max_depth`;
-    #     depth of the root node is zero;
+    #   - when `args.max_depth` is not `None`, its depth must be less than `args.max_depth`
+    #     (depth of the root node is zero);
+    #   - when `args.max_leaves` is not `None`, there are less than `args.max_leaves` leaves
+    #     (a leaf is a tree node without children);
     #   - there are at least `args.min_to_split` corresponding instances;
     #   - the criterion value is not zero.
     #
     # - When `args.max_leaves` is `None`, use recursive (left descendants first, then
     #   right descendants) approach, splitting every node if the constraints are valid.
-    #   Otherwise (when `args.max_leaves` is not `None`), always split a node where the
+    #   Otherwise (when `args.max_leaves` is not `None`), repeatably split a leaf where the
     #   constraints are valid and the overall criterion value ($c_left + c_right - c_node$)
     #   decreases the most. If there are several such nodes, choose the one
     #   which was created sooner (a left child is considered to be created
