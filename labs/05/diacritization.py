34c4d08fef1130ef4a03bc003ead2046372eaa66
Milan Straka <milan@strakovi.com>
2023-10-30 14:30:28 +0100

    Add assignments of the lecture 5.

---
+++ labs/05/diacritization.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import argparse
+import lzma
+import os
+import pickle
+import sys
+from typing import Optional
+import urllib.request
+
+import numpy as np
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--predict", default=None, type=str, help="Path to the dataset to predict")
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--model_path", default="diacritization.model", type=str, help="Model path")
+
+
+class Dataset:
+    LETTERS_NODIA = "acdeeinorstuuyz"
+    LETTERS_DIA = "áčďéěíňóřšťúůýž"
+
+    # A translation table usable with `str.translate` to rewrite characters with dia to the ones without them.
+    DIA_TO_NODIA = str.maketrans(LETTERS_DIA + LETTERS_DIA.upper(), LETTERS_NODIA + LETTERS_NODIA.upper())
+
+    def __init__(self,
+                 name="fiction-train.txt",
+                 url="https://ufal.mff.cuni.cz/~courses/npfl129/2324/datasets/"):
+        if not os.path.exists(name):
+            print("Downloading dataset {}...".format(name), file=sys.stderr)
+            licence_name = name.replace(".txt", ".LICENSE")
+            urllib.request.urlretrieve(url + licence_name, filename=licence_name)
+            urllib.request.urlretrieve(url + name, filename=name)
+
+        # Load the dataset and split it into `data` and `target`.
+        with open(name, "r", encoding="utf-8-sig") as dataset_file:
+            self.target = dataset_file.read()
+        self.data = self.target.translate(self.DIA_TO_NODIA)
+
+
+def main(args: argparse.Namespace) -> Optional[str]:
+    if args.predict is None:
+        # We are training a model.
+        np.random.seed(args.seed)
+        train = Dataset()
+
+        # TODO: Train a model on the given dataset and store it in `model`.
+        model = ...
+
+        # Serialize the model.
+        with lzma.open(args.model_path, "wb") as model_file:
+            pickle.dump(model, model_file)
+
+    else:
+        # Use the model and return test set predictions.
+        test = Dataset(args.predict)
+
+        with lzma.open(args.model_path, "rb") as model_file:
+            model = pickle.load(model_file)
+
+        # TODO: Generate `predictions` with the test set predictions. Specifically,
+        # produce a diacritized `str` with exactly the same number of words as `test.data`.
+        predictions = ...
+
+        return predictions
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+    main(args)
---
+++ lectures/lecture05.md
@@ -0,0 +1,4 @@
+### Lecture: 5. Lecture 5
+#### Date: Oct 31
+#### Lecture assignment: multilabel_classification_sgd
+#### Lecture assignment: diacritization
